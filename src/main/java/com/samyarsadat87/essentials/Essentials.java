7839a923d99d5786699e82e9f91b083860ed4fd7
Samyar Sadat Akhavi <samyarsadat87@gmail.com>
2021-12-29 13:08:37 +0300

    Updated changelog.

--- src/main/java/com/samyarsadat87/essentials/Essentials.java
+++ src/main/java/com/samyarsadat87/essentials/Essentials.java
@@ -1,20 +1,32 @@
 // Copyright 2021 Samyar Sadat Akhavi
 // Written by Samyar Sadat Akhavi
-// Gigamod, a mod by Gigawhat
+// Gigawhat Essentials, a mod by Gigawhat
 
 package com.samyarsadat87.essentials;
+
+import java.lang.reflect.AnnotatedWildcardType;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import net.minecraft.inventory.Inventory;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.ListNBT;
+import net.minecraft.util.SharedConstants;
+import net.minecraft.world.storage.DimensionSavedDataManager;
+import net.minecraft.world.storage.WorldSavedData;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.event.world.WorldEvent.Save;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import com.samyarsadat87.essentials.world_data.SaveWarpData;
 
 @Mod(Essentials.MOD_ID)
 @OnlyIn(Dist.DEDICATED_SERVER)
@@ -27,14 +39,26 @@ public class Essentials
     {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        LOGGER.info("Gigawhat Server Essentials Mod Loading...");
         LOGGER.info("Pre-init stage");
 
         bus.addListener(this::setup);
 
         MinecraftForge.EVENT_BUS.register(this);
+
+        LOGGER.info("Attemting to save example warp...");
+
+        LOGGER.info("Warp saved.");
+
+        ListNBT listnbt = new ListNBT();
+        CompoundNBT compoundnbt = new CompoundNBT();
+        SaveWarpData wp = new SaveWarpData();
+
+        compoundnbt.
+        compoundnbt.put("warpcoords", wp.save(new CompoundNBT()));
     }
 
-    private void setup(final FMLCommonSetupEvent event) 
+    private void setup(final FMLCommonSetupEvent event)
     {
 
     }
@@ -42,5 +66,5 @@ public class Essentials
     public static Logger getLOGGER() 
     {
         return LOGGER;
-    }
+    }  
 }
---
+++ src/main/java/com/samyarsadat87/essentials/world_data/SaveWarpData.java
@@ -0,0 +1,40 @@
+package com.samyarsadat87.essentials.world_data;
+
+import java.io.IOException;
+
+import com.samyarsadat87.essentials.Essentials;
+
+import io.netty.util.ResourceLeakDetector.Level;
+import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
+import it.unimi.dsi.fastutil.longs.LongSet;
+import net.minecraft.item.SaddleItem;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.CompressedStreamTools;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.SharedConstants;
+import net.minecraft.world.World;
+import net.minecraft.world.storage.DimensionSavedDataManager;
+import net.minecraft.world.storage.WorldSavedData;
+
+public class SaveWarpData extends WorldSavedData
+{
+    public SaveWarpData() 
+    {
+        super("warpdata");  
+    }
+
+    @Override
+    public void load(CompoundNBT nbt) 
+    {
+        if(nbt.contains("x"))
+        {
+            nbt.getList("x", 0);
+        }
+    }
+
+    @Override
+    public CompoundNBT save(CompoundNBT nbt) 
+    {
+        return nbt;
+    }
+}
