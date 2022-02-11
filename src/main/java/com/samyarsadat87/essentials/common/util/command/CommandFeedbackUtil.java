3496a956512deec580bfa15db07f0122f95d22df
Samyar Sadat Akhavi <samyarsadat87@gmail.com>
2022-02-11 23:21:11 +0300

    Updated Copyright

--- src/main/java/com/samyarsadat87/essentials/common/util/command/CommandFeedbackUtil.java
+++ src/main/java/com/samyarsadat87/essentials/common/util/command/CommandFeedbackUtil.java
@@ -13,7 +13,7 @@
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  * 
  * For further inquiries you can contact the Gigawhat Programming Team via e-mail:
  * contact.gpt@gigawhat.net
